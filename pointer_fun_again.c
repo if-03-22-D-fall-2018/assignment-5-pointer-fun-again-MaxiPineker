e25ec9068582299548d68bccee45aa76b7dca8c7
MaxiPineker <maxi.pineker@gmail.com>
2018-10-18 11:43:10 +0200

    first steps

---
+++ pointer_fun_again.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_struct(PlayStruct ps,PlayStruct* pps);
+
+
+struct PlayStruct{
+  int int_value;
+  double double_value;
+  char a_string[64];
+}
+
+int main(int argc, char const *argv[]) {
+  PlayStruct* play_struct_pointer;
+  return 0;
+}
+ void print_struct(PlayStruct ps,PlayStruct* pps){
+
+ }
