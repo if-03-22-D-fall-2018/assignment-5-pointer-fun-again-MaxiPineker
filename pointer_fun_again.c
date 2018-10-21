f475945b5e002ace8755f35b76946b4d196c30f6
Maxi Pineker <maxi.pineker@gmail.com>
2018-10-21 20:03:50 +0200

    finished program

--- pointer_fun_again.c
+++ pointer_fun_again.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
-
-void print_struct(PlayStruct ps,PlayStruct* pps);
-
+#include <string.h>
 
 struct PlayStruct{
   int int_value;
   double double_value;
   char a_string[64];
-}
+};
+
+void print_struct(struct PlayStruct ps,struct PlayStruct* pps);
+void change_struct(struct PlayStruct ps,struct PlayStruct* pps);
+void print_string(char string_to_print[]);
+void change_string(char string1[], char* p_string[]);
+
 
 int main(int argc, char const *argv[]) {
-  PlayStruct* play_struct_pointer;
+  struct PlayStruct play_struct ={6,6.5, "Maxi"};
+  struct PlayStruct *play_struct_pointer = &play_struct;
+  char another_string[16] = "New string";
+  char *another_string_p = another_string;
+
+  print_struct(play_struct, play_struct_pointer);
+  change_struct(play_struct, play_struct_pointer);
+  print_struct(play_struct, play_struct_pointer);
+
+  print_string(play_struct.a_string);
+  change_string(play_struct.a_string, &another_string_p);
+  print_string(play_struct.a_string);
+  print_string(another_string_p);
+
   return 0;
 }
- void print_struct(PlayStruct ps,PlayStruct* pps){
-
+ void print_struct(struct PlayStruct ps,struct PlayStruct* pps){
+   printf("%d %lf %s\n",ps.int_value ,ps.double_value, ps.a_string);
+   printf("%d %lf %s\n",pps->int_value ,pps->double_value, pps->a_string);
  }
+ void change_struct(struct PlayStruct ps,struct PlayStruct* pps){
+   ps.int_value = 18;
+   ps.double_value = 18.5;
+   pps->int_value = 18;
+   pps->double_value = 18.5;
+ }
+void print_string(char string_to_print[]) {
+  printf("%s\n", string_to_print);
+}
+void change_string(char string1[], char* p_string[]) {
+  string1[2] = "0";
+  p_string[1] = "0";
+}
